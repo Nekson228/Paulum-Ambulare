c7c32f573061831ad72b6e483d1791791b03a887
NeePaw <bangerwockeez@gmail.com>
2020-12-12 20:04:14 +0200

    added collisions between character and obstacles

--- main.py
+++ main.py
@@ -1,32 +1,66 @@
 import pygame
 
 SPEED = 10  # константа скорости персонажа
+IMAGES = {
+    'character': 'resources/character.png',
+    'obstacle': 'resources/obstacle.png'
+}
 
 
 class Character(pygame.sprite.Sprite):
-    image = pygame.image.load('resources/character.png')
+    image = pygame.image.load(IMAGES['character'])
 
-    def __init__(self, group):
+    def __init__(self, group, x, y):
         super().__init__(group)
         self.image = Character.image
         self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
 
     def move(self, x, y):
         self.rect.x += x
         self.rect.y += y
+        collided_sprite = pygame.sprite.spritecollideany(self, obstacles)
+        if collided_sprite:
+            if x > 0:
+                self.rect.x = collided_sprite.rect.x - self.rect.w
+            elif x < 0:
+                self.rect.x = collided_sprite.rect.x + collided_sprite.rect.w
+            if y > 0:
+                self.rect.y = collided_sprite.rect.y - self.rect.h
+            elif y < 0:
+                self.rect.y = collided_sprite.rect.y + collided_sprite.rect.h
+
+
+class Obstacle(pygame.sprite.Sprite):
+    image = pygame.image.load(IMAGES['obstacle'])
+
+    def __init__(self, group, x, y, w, h):
+        super().__init__(group)
+        self.image = Obstacle.image
+        self.image = pygame.transform.scale(self.image, (w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
 
 
 if __name__ == '__main__':
     # Инициализация #
     pygame.init()
     pygame.display.set_caption('Untitled Nekit Game')
-    size = width, height = 1280, 640
+    size = width, height = 640, 640
     screen = pygame.display.set_mode(size)
     pygame.mouse.set_visible(False)
 
     # Спрайты #
     all_sprites = pygame.sprite.Group()
-    character = Character(all_sprites)
+    obstacles = pygame.sprite.Group()
+    character = Character(all_sprites, width // 2, height // 2)
+
+    obs1 = Obstacle(obstacles, 200, 50, 50, 100)
+    obs2 = Obstacle(obstacles, 250, 50, 100, 50)
+    obs3 = Obstacle(obstacles, 350, 50, 100, 50)
+    obs4 = Obstacle(obstacles, 450, 50, 50, 100)
+    obs5 = Obstacle(obstacles, 450, 100, 50, 250)
+    obs6 = Obstacle(obstacles, 200, 100, 50, 250)
 
     clock = pygame.time.Clock()
     fps = 30  # частота обновления экрана (кадров в секунду)
@@ -54,5 +88,7 @@ if __name__ == '__main__':
         # Обновление экрана #
         screen.fill(pygame.Color('black'))
         all_sprites.draw(screen)
+        obstacles.draw(screen)
+        obstacles.update()
         pygame.display.flip()
     pygame.quit()
