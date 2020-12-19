888bf031179ccbf95ea0fa335cca0cc347ee9e61
pashkevich <pavsav.04@gmail.com>
2020-12-19 14:44:48 +0500

    added level generating from *.txt file, test map, tile pic and updated movement and tiles

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
 import pygame
+import sys
+import os
 
 SPEED = 10  # константа скорости персонажа
 JUMP_HEIGHT = 6  # константа высоты прыжка персонажа
 ASSETS = {  # используемые ассеты
-    'character': 'resources/character.png',
-    'obstacle': 'resources/obstacle.png'
+    'character': 'character.png',
+    'tile': 'tile.png',
+    'obstacle': 'obstacle.png'
 }
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('resources', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def load_level(filename):  # обработка файла для реализации сборки уровня
+    filename = "resources/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):  # генерация уровня из обработанного текстового файла
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('tile', x, y)
+            elif level[y][x] == '#':
+                Tile('obstacle', x, y)
+            elif level[y][x] == '@':
+                Tile('tile', x, y)
+                new_player = Character(x, y)
+    return new_player, x, y
+
+
 class Character(pygame.sprite.Sprite):
-    image = pygame.image.load(ASSETS['character'])
+    image = load_image(ASSETS['character'])
 
     def __init__(self, x, y):
-        super().__init__(all_sprites)
+        super().__init__(player_group, all_sprites)
         self.image = Character.image
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = x, y
+        self.rect = self.image.get_rect().move(tile_width * x, tile_height * y)
 
         self.jump = False
         self.jump_delta = JUMP_HEIGHT
@@ -35,15 +74,16 @@ class Character(pygame.sprite.Sprite):
                 self.rect.y = collided_sprite.rect.y + collided_sprite.rect.h
 
 
-class Obstacle(pygame.sprite.Sprite):
-    image = pygame.image.load(ASSETS['obstacle'])
-
-    def __init__(self, x, y, w, h):
-        super().__init__(all_sprites)
-        self.image = Obstacle.image
-        self.image = pygame.transform.scale(self.image, (w, h))
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = x, y
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, x, y):
+        if tile_type == 'obstacle':
+            super().__init__(obstacles, all_sprites)
+        else:
+            super().__init__(tiles_group, all_sprites)
+        image = load_image(ASSETS[tile_type])
+        self.image = image
+        self.image = pygame.transform.scale(self.image, (tile_width, tile_height))
+        self.rect = self.image.get_rect().move(tile_width * x, tile_height * y)
 
 
 class Camera:
@@ -73,20 +113,18 @@ if __name__ == '__main__':
 
     # Спрайты #
     all_sprites = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
     obstacles = pygame.sprite.Group()
-    character = Character(width // 2, height // 2)
 
-    obs1 = Obstacle(200, 50, 50, 100)
-    obs2 = Obstacle(250, 50, 100, 50)
-    obs3 = Obstacle(350, 50, 100, 50)
-    obs4 = Obstacle(450, 50, 50, 100)
-    obs5 = Obstacle(450, 100, 50, 250)
-    obs6 = Obstacle(200, 100, 50, 250)
+    tile_width = tile_height = 30  # задаем размеры(можно любой, этот для теста)
+
+    character, level_x, level_y = generate_level(load_level('test_map.txt'))
 
     clock = pygame.time.Clock()
     fps = 30  # частота обновления экрана (кадров в секунду)
 
-    camera = Camera() # объект камеры
+    camera = Camera()  # объект камеры
 
     # Основной цикл #
     run = True
@@ -99,11 +137,6 @@ if __name__ == '__main__':
 
         # Передвижение персонажа #
         keys = pygame.key.get_pressed()
-        if not character.jump:
-            if keys[pygame.K_UP] or keys[pygame.K_w]:
-                character.move(0, -SPEED)
-            if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                character.move(0, SPEED)
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             character.move(-SPEED, 0)
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
@@ -123,8 +156,8 @@ if __name__ == '__main__':
         # обновляем положение спрайтов
         for sprite in all_sprites:
             camera.apply(sprite)
-        all_sprites.draw(screen)
+        tiles_group.draw(screen)
         obstacles.draw(screen)
-        obstacles.update()
+        player_group.draw(screen)
         pygame.display.flip()
     pygame.quit()
